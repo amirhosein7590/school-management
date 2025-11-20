47189af3b7055b04c0c33b5b6f4a3d09621c2c32
amirhosein <amirhosein7590@gmail.com>
2025-11-20 09:27:48 +0330

    check ban status and subscription expire time in refresh token api

--- src/pages/api/auth/refresh.js
+++ src/pages/api/auth/refresh.js
@@ -9,13 +9,11 @@ import { serialize } from "cookie";
 export default async function RefreshToken(req, res) {
   const { refreshToken } = req.cookies;
   if (!refreshToken) {
-    return res
-      .status(401)
-      .json({
-        isLoggined: false,
-        success: false,
-        error: "لطفا وارد حساب کاربری خود شوید",
-      });
+    return res.status(401).json({
+      isLoggined: false,
+      success: false,
+      error: "لطفا وارد حساب کاربری خود شوید",
+    });
   }
 
   const { nationalCode, role } = verifyToken(refreshToken);
@@ -23,6 +21,25 @@ export default async function RefreshToken(req, res) {
     return res
       .status(403)
       .json({ error: "دسترسی نامعتبر است", success: false });
+  }
+  const user = await findUserByProp("nationalCode", nationalCode);
+
+  if (!user) {
+    return res
+      .status(401)
+      .json({ error: "شما در سایت ثبت نام نیستید", success: false });
+  }
+
+  if (user.isBanned) {
+    if (user.expTime < Date.now()) {
+      return res
+        .status(403)
+        .json({ error: "اشتراک شما به پایان رسیده است", success: false });
+    }
+
+    return res
+      .status(403)
+      .json({ error: "حساب کاربری شما بن شده است", success: false });
   }
   const newToken = generateToken({ nationalCode, role });
   const newRefreshToken = generateRefreshToken({ nationalCode, role });
