5cae0c7a09c4cbe9ec89922b6547393d07cde7fa
amirhosein <amirhosein7590@gmail.com>
2025-11-25 10:15:20 +0330

    add groupAdding for students

--- src/models/student.js
+++ src/models/student.js
@@ -9,25 +9,21 @@ const studentSchema = new mongoose.Schema({
     type: mongoose.Types.ObjectId,
     required: true,
     ref: "School",
-    unique: true,
   },
   class: {
     type: mongoose.Types.ObjectId,
     required: false,
     ref: "Class",
-    unique: true,
   },
   teacher: {
     type: mongoose.Types.ObjectId,
     required: false,
     ref: "Teacher",
-    unique: true,
   },
   manager: {
     type: mongoose.Types.ObjectId,
     ref: "Manager",
     required: true,
-    unique: true,
   },
   birthDay: { type: Date, required: true },
   grade: {
@@ -36,6 +32,11 @@ const studentSchema = new mongoose.Schema({
     enum: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
   },
 });
+
+studentSchema.index({ manager: 1 });
+studentSchema.index({ school: 1 });
+studentSchema.index({ teacher: 1 });
+studentSchema.index({ class: 1 });
 
 const studentModel =
   mongoose.models.Student || mongoose.model("Student", studentSchema);
---
+++ src/pages/api/students/groupAdding.js
@@ -0,0 +1,90 @@
+import RBAC from "@/utils/RBAC";
+import managerModel from "@/models/manager";
+import connectToDb from "@/utils/db";
+import studentModel from "@/models/student";
+
+export default async function GroupAdding(req, res) {
+  if (req.method !== "POST") {
+    return res
+      .status(400)
+      .json({ error: "این درخواست مجاز نیست", success: false });
+  }
+
+  const auth = await RBAC(req, res, ["owner", "manager"], { status: false });
+  if (!auth) return;
+
+  const { nationalCode } = auth;
+
+  try {
+    await connectToDb();
+    const manager = await managerModel.findOne({ nationalCode });
+    if (!manager) {
+      return res.status(403).json({
+        error: "شما مجاز به انجام این عملیات نیستید",
+        success: false,
+      });
+    }
+
+    if (!manager.actionsPermissions?.createStudent) {
+      return res.status(403).json({
+        error: "این عملیات از سوی مالک محدود شده است",
+        success: false,
+      });
+    }
+    const { students } = req.body;
+    if (!students || !Array.isArray(students) || students.length === 0) {
+      return res
+        .status(422)
+        .json({ error: "فرمت یا مقدار فیلد نامعتبر است", success: false });
+    }
+
+    const exceptedProps = [
+      "firstName",
+      "lastName",
+      "nationalCode",
+      "parentPhone",
+      "birthDay",
+      "grade",
+    ];
+    for (const s of students) {
+      const isValid = exceptedProps.every((key) => s[key]);
+      if (!isValid) {
+        return res
+          .status(422)
+          .json({ error: "فیلد یا مقدار نامعتبر است", success: false });
+      }
+    }
+    const nationalCodes = students.map((s) => s.nationalCode);
+    const parentPhones = students.map((s) => s.parentPhone);
+    const duplicates = await studentModel.find({
+      $or: [
+        { nationalCode: { $in: nationalCodes } },
+        { parentPhone: { $in: parentPhones } },
+      ],
+    });
+    if (duplicates.length > 0) {
+      return res.status(409).json({
+        error: "دانش آموز تکراری وجود دارد",
+        success: false,
+      });
+    }
+
+    const preparedStudents = await Promise.all(
+      students.map(async (s) => ({
+        ...s,
+        school: manager.school,
+        manager: manager._id,
+      }))
+    );
+    await studentModel.insertMany(preparedStudents, {
+      ordered: false,
+    });
+
+    return res.status(201).json({
+      message: "دانش آموزان با موفقیت ایجاد شدند",
+      success: true,
+    });
+  } catch (error) {
+    return res.status(500).json({ error: "خطای ناشناخته", dbError : error, success: false });
+  }
+}
